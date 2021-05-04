e7154ed935fefac4018f59cc937872fa2e95e7bd
naventis <nikolamazur56@gmail.com>
2021-05-04 13:40:18 +0200

    profil + vänner (behöver lite CSS arbete)

--- src/components/Friends.js
+++ src/components/Friends.js
@@ -1,9 +1,37 @@
 import React from 'react'
+import {BrowserRouter as Router, Link, Switch } from 'react-router-dom'
+import { Container, Card, Row, Col } from "react-bootstrap";
+import johnnyb from '../profileicons/johnnyb.svg';
+import ronnyb from '../profileicons/ronnyb.svg';
+import kenny from '../profileicons/kenny.svg';
+import rocky from '../profileicons/rocky.svg';
+import sonny from '../profileicons/sonny.svg';
 
 export default function Friends() {
     return (
-        <div>
-            
-        </div>
+            <Container class="friends">
+                <Card.Text>Mina vänner</Card.Text>
+                <Switch>
+                    <div><Link to="/Johnny"><img src={johnnyb}alt="Johnny" /></Link>  
+                    <br/>Johnny</div>
+                </Switch>
+                <Switch>
+                    <div><Link to="/Ronny"><img src={ronnyb} alt="Ronny"/></Link>
+                    <br/>Ronny</div>
+                </Switch>
+                <Switch>
+                    <div><Link to="/Kenny"><img src={kenny} alt="Kenny"/></Link>
+                    <br/>Kenny</div>
+                </Switch>
+                <Switch>
+                    <div><Link to="/Rocky"><img src={rocky} alt="Rocky"/></Link>
+                    <br/>Rocky</div>
+                </Switch>
+                <Switch>
+                    <div><Link to="/Sonny"><img src={sonny} alt="Sonny"/></Link>
+                    <br/>Sonny</div>
+                </Switch>
+            </Container>
+        
     )
 }
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,11 +1,40 @@
 import React from 'react'
-import { Button } from "react-bootstrap";
+import { Button, Card } from "react-bootstrap";
+import {BrowserRouter as Router, Link, Switch } from 'react-router-dom'
+import manne from '../profileicons/manne.svg';
+import johnny from '../profileicons/johnny.svg';
+import ronny from '../profileicons/ronny.svg';
 
 
 export default function Profile({handleLogout}) {
   return (
-    <div>
-      <Button onClick={handleLogout}>Logga ut</Button>
-    </div>
+      <>
+            <div className="profilecard">
+                <Card>
+                    <Card.Body>
+                        <Card.Title>Profil</Card.Title>
+                        <Card.Img class="img" variant="top" src={manne} alt="Manne profile icon" />
+                        <Card.Text class="name">Manne 36</Card.Text>
+                        <Card.Subtitle>
+                            Tja! Manne här! Jag gillar att käka god mat och gamear gärna när jag
+                            får tid över. Sugen på en bärs eller ska vi ba käka lunch? Hör av
+                            dig!
+                        </Card.Subtitle>
+                        <Card.Text class="buddies">Hänger ofta med</Card.Text>
+                    </Card.Body>
+                </Card>
+                    <Switch>
+                    <div><Link to="/Johnny"><img src={johnny}alt="Johnny" /></Link>  Johnny the Trucker</div>
+                    </Switch>
+                    <Switch>
+                    <div><Link to="/Ronny"><img src={ronny} alt="Ronny"/></Link>  Ronny Booster</div>
+                    </Switch>
+
+                </div>
+                
+        <div>
+            <Button onClick={handleLogout}>Logga ut</Button>
+        </div>
+    </>
   );
 }
