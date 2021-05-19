da4eb15caf5cb21497da3645945d6d1aceebf54a
naventis <72204459+naventis@users.noreply.github.com>
2021-05-19 22:47:43 +0200

    Update Hero.js

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,9 +1,7 @@
 import React from "react";
-import { Button } from "react-bootstrap";
 import { useMobile } from "./useMobile";
 import Navbar from "../navbar/navbar";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { Nav, Container } from "react-bootstrap";
 import EventList from "./EventList";
 import EventForm from "./EventForm";
 import Profile from "./Profile";
@@ -12,44 +10,41 @@ import Activities from "./Activities";
 import Johnny from "../profiles/Johnny";
 
 const Hero = ({ handleLogout }) => {
-  const isMobile = useMobile();
+    const isMobile = useMobile();
+ 
+    return (
+        <Router>
+            {!isMobile && <Navbar />}
+            {isMobile && (
+                <div style={{ position: "fixed", bottom: "0px", width: "100%" }} >
+                    <Navbar />{" "}
+                </div>
+            )}
 
-  return (
-    <Router>
-      {!isMobile && <Navbar />}
-      {isMobile && (
-        <div style={{ position: "fixed", bottom: "0px", width: "100%" }}>
-          <Navbar />{" "}
-        </div>
-      )}
-
-      {/*Här finns Routes till de olika komponenter*/}
-      <Switch>
-        {/*TODO: Lägga in route för path"/" alltså hemsida*/}
-        <Route path="/activities">
-          <Activities />
-        </Route>
-        <Route path="/calender">
-          <EventList />
-        </Route>
-        <Route path="/addevent">
-          <EventForm />
-        </Route>
-        <Route path="/profile">
-          <Profile />
-        </Route>
-        <Route path="/friends">
-          <Friends />
-        </Route>
-        <Route path="/Johnny">
-          <Johnny />
-        </Route>
-      </Switch>
-
-      <Button variant="link" onClick={handleLogout}>
-        Logga ut
-      </Button>
-    </Router>
-  );
+            {/*Här finns Routes till de olika komponenter*/}
+            <Switch>
+                {/*TODO: Lägga in route för path"/" alltså hemsida*/}
+                <Route path="/activities">
+                    <Activities />
+                </Route>
+                <Route path="/calender">
+                    <EventList />
+                </Route>
+                <Route path="/addevent">
+                    <EventForm />
+                </Route>
+                <Route path="/profile">
+                    <Profile handleLogout={handleLogout}/>
+                </Route>
+                <Route path="/friends">
+                    <Friends />
+                </Route>
+                <Route path="/Johnny">
+                    <Johnny />
+                </Route>
+            </Switch>
+ 
+        </Router>
+    );
 };
 export default Hero;
