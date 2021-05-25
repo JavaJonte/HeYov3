e3c9268681d07ca486170578447839c029daca59
Jonathan Wikman <73289628+JavaJonte@users.noreply.github.com>
2021-05-26 00:00:56 +0200

    started modifying landing page

---
+++ src/components/HomePage.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+
+export default function HomePage() {
+  return (
+    <Container>
+      <h1 className="landing-h1">
+        Friends,<br></br> {/* FIXA CSSEN */}
+        old & new <br></br>
+        lets hang out!
+      </h1>
+      <h2 className="landing-h2">
+        HEYO är tjänsten för dig som vill hålla kontakten med gamla vänner och
+        träffa nya.
+      </h2>
+      <p className="right-quote">
+        “Så sjukt kul sätt att kunna planera saker med vänner på.”
+      </p>
+
+      <p className="left-quote">
+        “Sen jag skaffade Heyo! har jag träffat så många nya härliga människor.”
+      </p>
+
+      <Container>
+        <p className="footer">
+          Om HEYO! <br></br>
+          Kontakt: heyo@info.com <br></br>{" "}
+          {/*Styla denna och padda in den snyggt så den ser lik ut mot prototyp*/}
+          copiright blabla 2021
+        </p>
+      </Container>
+    </Container>
+  );
+}
--- src/components/onBoarding.js
+++ src/components/onBoarding.js
@@ -3,57 +3,60 @@ import { Button, Container } from "react-bootstrap";
 import fire from "../fire";
 import Hero from "./Hero";
 import LoginSystem from "./LoginSystem";
+import HomePage from "./HomePage"
 
 export const OnBoarding = () => {
-    const [hasAccount, setHasAccount] = useState(false);
-    const [showLoginForm, setShowLoginForm] = useState(false);
-    const [showOnBoarding, setOnBoarding] = useState(true);
-    const [showHero, setShowHero] = useState(window.localStorage.getItem("user"));
+  const [hasAccount, setHasAccount] = useState(false);
+  const [showLoginForm, setShowLoginForm] = useState(false);
+  const [showOnBoarding, setOnBoarding] = useState(true);
+  const [showHero, setShowHero] = useState(window.localStorage.getItem("user"));
 
-    const handleLogin = () => {
-        setOnBoarding(false);
-        setHasAccount(true);
-        setShowLoginForm(true);
-    };
-    
-    const handleRegisterLogin = () => {
-        setHasAccount(false);
-        setShowLoginForm(true);
-        setOnBoarding(false);
-    }
+  const handleLogin = () => {
+    setOnBoarding(false);
+    setHasAccount(true);
+    setShowLoginForm(true);
+  };
 
-    const handleLogout = () => {
-        fire.auth().signOut();
-        window.localStorage.removeItem("user");
-        window.location.href="/";
-      };
+  const handleRegisterLogin = () => {
+    setHasAccount(false);
+    setShowLoginForm(true);
+    setOnBoarding(false);
+  };
 
+  const handleLogout = () => {
+    fire.auth().signOut();
+    window.localStorage.removeItem("user");
+    window.location.href = "/";
+  };
 
-    const hideLoginForm = () => {
-        setShowLoginForm(false);
-        setOnBoarding(false);
-        setShowHero(true);
-    }
+  const hideLoginForm = () => {
+    setShowLoginForm(false);
+    setOnBoarding(false);
+    setShowHero(true);
+  };
 
-    const isLogin = window.localStorage.getItem("user");
+  const isLogin = window.localStorage.getItem("user");
 
-    return (
+  return (
     <>
-        {showOnBoarding && isLogin!="true" && <section className="login">
-        <Container className="loginContainer">
+      {showOnBoarding && isLogin != "true" && (
+        <section className="login">
+          <Container className="loginContainer">
             <Container className="btnContainer">
-                <div className="heyo">HEYO!</div>
-                <Button onClick={handleLogin}>&nbsp;&nbsp;&nbsp;LOGGA IN&nbsp;&nbsp;&nbsp;</Button>
-                <p>
-                    Har du inget konto? <br/>
-                    <span onClick={handleRegisterLogin}>
-                        Registrera dig här!
-                    </span>
-                </p>
-            </Container>
+              <div className="heyo">HEYO!</div>
+              <Button onClick={handleLogin}>
+                &nbsp;&nbsp;&nbsp;LOGGA IN&nbsp;&nbsp;&nbsp;
+              </Button>
+              <p>
+                Har du inget konto? <br />
+                <span onClick={handleRegisterLogin}>Registrera dig här!</span>
+              </p>
+              <HomePage />
+              </Container>
+              
           </Container>
         </section>
-      }
+      )}
       {showLoginForm && (
         <LoginSystem hasAccount={hasAccount} showLoginForm={hideLoginForm} />
       )}
